5ee9a9a33de3350e226b53573a4101a2eb9b59c8
Kantheti Pavankumar(12221270) <115073063+pavankumar689@users.noreply.github.com>
2025-12-30 09:16:56 +0530

    saturday Assessments

---
+++ saturdayAssessments/MediSureClinic/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class Medisure
+{
+    public static void Main(string[] args)
+    {
+        bool running = true;
+
+        
+        while (running)
+        {
+            Console.WriteLine("================== MediSure Clinic Billing ==================");
+            Console.WriteLine("1. Create New Bill (Enter Patient Details)");
+            Console.WriteLine("2. View Last Bill");
+            Console.WriteLine("3. Clear Last Bill");
+            Console.WriteLine("4. Exit");
+            Console.Write("Enter your option: ");
+
+            int choice;
+            if (!int.TryParse(Console.ReadLine(), out choice))
+            {
+                Console.WriteLine("Invalid input. Please enter a number.\n");
+                continue;
+            }
+
+            switch (choice)
+            {
+                case 1:
+                    PatientBill.CreateBill();
+                    break;
+
+                case 2:
+                    PatientBill.ViewLastBill();
+                    break;
+
+                case 3:
+                    PatientBill.ClearLastBill();
+                    break;
+
+                case 4:
+                    running = false;
+                    Console.WriteLine("\nThank you. Application closed normally.");
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid menu option. Please try again.\n");
+                    break;
+            }
+        }
+    }
+}
---
+++ saturdayAssessments/QuickMart/Transactions.cs
@@ -0,0 +1,13 @@
+class Transaction
+{
+    public string? InvoiceNo { get; set; }
+    public string? CustomerName { get; set; }
+    public string? ItemName { get; set; }
+    public int Quantity { get; set; }
+    public decimal PurchaseAmount { get; set; }
+    public decimal SellingAmount { get; set; }
+
+    public string? ProfitOrLossStatus { get; set; }
+    public decimal ProfitOrLossAmount { get; set; }
+    public decimal ProfitMarginPercent { get; set; }
+}
