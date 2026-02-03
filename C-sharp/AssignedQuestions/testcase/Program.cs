49477a0acffeeb68482793798577fa8fde6b130b
Kantheti Pavankumar(12221270) <115073063+pavankumar689@users.noreply.github.com>
2026-02-03 08:56:46 +0530

    assigned question

---
+++ C-sharp/AssignedQuestions/testcase/Program.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace BankAccountTests
+{
+  
+    public class UnitTest
+    {
+    
+        public void Test_Deposit_ValidAmount()
+        {
+            
+            Program account = new Program(100m);
+
+            
+            account.Deposit(50m);
+
+        
+            Assert.AreEqual(150m, account.Balance);
+        }
+
+    
+        public void Test_Deposit_NegativeAmount()
+        {
+        
+            Program account = new Program(100m);
+
+           
+            var ex = Assert.Throws<Exception>(() => account.Deposit(-10m));
+
+            Assert.AreEqual("Deposit amount cannot be negative", ex.Message);
+        }
+
+     
+        public void Test_Withdraw_ValidAmount()
+        {
+        
+            Program account = new Program(200m);
+
+           
+            account.Withdraw(50m);
+
+        
+            Assert.AreEqual(150m, account.Balance);
+        }
+
+       
+        public void Test_Withdraw_InsufficientFunds()
+        {
+            
+            Program account = new Program(100m);
+
+            
+            var ex = Assert.Throws<Exception>(() => account.Withdraw(150m));
+
+            Assert.AreEqual("Insufficient funds.", ex.Message);
+        }
+    }
+}
