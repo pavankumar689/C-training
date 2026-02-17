dcdb18da6ff97fbd0d9e911d3d9bf4367b0729ed
Kantheti Pavankumar(12221270) <115073063+pavankumar689@users.noreply.github.com>
2026-02-17 15:56:04 +0530

    genericlibrary catalog added

---
+++ C-sharp/saturdayAssessments/sat-feb-14/genericlibrarycatalog/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Book
+{
+    public string ISBN { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string Genre { get; set; }
+    public bool IsAvailable { get; set; } = true;
+}
+
+public class Catalog<T> where T : Book
+{
+    private List<T> _items = new();
+    private HashSet<string> _isbnSet = new();
+    private SortedDictionary<string, List<T>> _genreIndex = new();
+
+    public bool AddItem(T item)
+    {
+        if (_isbnSet.Contains(item.ISBN))
+            return false;
+
+        _isbnSet.Add(item.ISBN);
+        _items.Add(item);
+
+        if (!_genreIndex.ContainsKey(item.Genre))
+            _genreIndex[item.Genre] = new List<T>();
+
+        _genreIndex[item.Genre].Add(item);
+
+        return true;
+    }
+
+    public List<T> this[string genre]
+    {
+        get
+        {
+            return _genreIndex.ContainsKey(genre)
+                ? _genreIndex[genre]
+                : new List<T>();
+        }
+    }
+
+    public IEnumerable<T> FindBooks(Func<T, bool> predicate)
+    {
+        return _items.Where(predicate);
+    }
+}
